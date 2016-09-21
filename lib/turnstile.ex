c34765b5962366c3389dcd4ff4198620578eff25
Andy Mention <amention@gmail.com>
2016-09-20 21:57:25 -0600

    mellow refactor

--- lib/turnstile.ex
+++ lib/turnstile.ex
@@ -4,7 +4,7 @@ defmodule Turnstile do
   # Client API
 
   def start_link do
-    GenServer.start_link(__MODULE__, {:locked, 0})
+    GenServer.start_link(__MODULE__, 0)
   end
 
   def is_locked?(pid) do
@@ -21,21 +21,19 @@ defmodule Turnstile do
 
   # Server Callbacks
 
-  def handle_call(:is_locked?, _from, {:unlocked, _tokens} = state),
-    do: {:reply, false, state}
+  def handle_call(:is_locked?, _from, tokens) do
+    {:reply, locked?(tokens), tokens}
+  end
 
-  def handle_call(:is_locked?, _from, {:locked, _tokens} = state),
-    do: {:reply, true, state}
+  def handle_cast(:insert_coin, tokens),
+    do: {:noreply, (tokens + 1)}
 
-  def handle_cast(:insert_coin, {_locked_state, tokens}),
-    do: {:noreply, {:unlocked, (tokens + 1)}}
+  def handle_call(:push, _from, 0),
+    do: {:reply, {:error, "locked"}, 0}
 
-  def handle_call(:push, _from, {:unlocked, tokens}),
-    do: {:reply, :ok, new_state(tokens - 1)}
+  def handle_call(:push, _from, tokens),
+    do: {:reply, :ok, (tokens - 1)}
 
-  def handle_call(:push, _from, {:locked, 0}),
-    do: {:reply, {:error, "locked"}, {:locked, 0}}
-
-  defp new_state(0), do: {:locked, 0}
-  defp new_state(tokens), do: {:unlocked, tokens}
+  defp locked?(tokens) when tokens  == 0, do: true
+  defp locked?(tokens) when tokens > 0, do: false
 end
